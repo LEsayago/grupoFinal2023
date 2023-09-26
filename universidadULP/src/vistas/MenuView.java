891f72b83a9207e414682e2744b61c8a5087db16
Enzo021 <enzopabone1@gmail.com>
2023-09-25 22:15:29 -0300

    Logo ULP + correción tamaños
    Agregué un panel azul al menú principal para poner el logo de la ULP. También fui cambiando los tamaños de cada ventana para que encajaran, mañana seguimos y agregamos imágenes, etc.

--- universidadULP/src/vistas/MenuView.java
+++ universidadULP/src/vistas/MenuView.java
@@ -44,6 +44,8 @@ public class MenuView extends javax.swing.JFrame {
     private void initComponents() {
 
         jDEscritorioMenu = new javax.swing.JDesktopPane();
+        jPanel1 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
         jMenuBar1 = new javax.swing.JMenuBar();
         jMAlumno = new javax.swing.JMenu();
         jMFormuAlumno = new javax.swing.JMenuItem();
@@ -62,11 +64,32 @@ public class MenuView extends javax.swing.JFrame {
         jDEscritorioMenu.setLayout(jDEscritorioMenuLayout);
         jDEscritorioMenuLayout.setHorizontalGroup(
             jDEscritorioMenuLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 874, Short.MAX_VALUE)
+            .addGap(0, 545, Short.MAX_VALUE)
         );
         jDEscritorioMenuLayout.setVerticalGroup(
             jDEscritorioMenuLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 572, Short.MAX_VALUE)
+            .addGap(0, 0, Short.MAX_VALUE)
+        );
+
+        jPanel1.setBackground(new java.awt.Color(13, 63, 119));
+
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/fotitos/ulp logo azul.png"))); // NOI18N
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap(21, Short.MAX_VALUE)
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 412, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(14, 14, 14))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(21, 21, 21)
+                .addComponent(jLabel1)
+                .addContainerGap(423, Short.MAX_VALUE))
         );
 
         jMAlumno.setText("Alumno");
@@ -145,15 +168,18 @@ public class MenuView extends javax.swing.JFrame {
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jDEscritorioMenu)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jDEscritorioMenu, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jDEscritorioMenu)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jPanel1, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jDEscritorioMenu))
                 .addContainerGap())
         );
 
@@ -306,6 +332,7 @@ public class MenuView extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JDesktopPane jDEscritorioMenu;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JMenu jMAdmin;
     private javax.swing.JMenu jMAlumno;
     private javax.swing.JMenuItem jMAlumnoPorMateria;
@@ -317,5 +344,6 @@ public class MenuView extends javax.swing.JFrame {
     private javax.swing.JMenu jMSalir;
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JMenuItem jMinscripcion;
+    private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
 }
